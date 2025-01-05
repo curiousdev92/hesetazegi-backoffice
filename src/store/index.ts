16600facc75c14fad38b3630c65b8efb35c85aad
m.naderkhani <m.naderkhani93@gmail.com>
2025-01-06 00:08:25 +0330

    remove extra fishSlice from zustand

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,11 @@
 import { create } from "zustand";
 import createBearSlice from "./bearSlice";
-import createFishSlice from "./fishSlice";
 
 interface BearSlice {
   bears: number;
   addBear: () => void;
 }
 
-interface FishSlice {
-  fishes: number;
-  addFish: () => void;
-}
-
-export const useStore = create<BearSlice & FishSlice>()((...a) => ({
+export const useStore = create<BearSlice>()((...a) => ({
   ...createBearSlice(...a),
-  ...createFishSlice(...a),
 }));
