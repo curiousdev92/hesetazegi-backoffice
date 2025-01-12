7435e37557e3bb1a8c4760293f430b0d9eb138bf
front-hesetazegi <fronthesetazegi@gmail.com>
2025-01-12 20:28:07 +0330

    add loader to routing - phase 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import { BrowserRouter } from "react-router";
-import DynamicNavigation from "./routes/navigations";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useLoaderData,
+} from "react-router";
+import { getCookie } from "./utils/cookies";
+import { GET_MENU_URL } from "./utils/urls";
+const token = getCookie("bo-tkn");
 
 function App() {
-  return (
-    <BrowserRouter>
-      <DynamicNavigation />
-    </BrowserRouter>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      Component: () => {
+        let data = useLoaderData();
+        console.log(data);
+        return <div>Home</div>;
+      },
+      loader: async () =>
+        await fetch(GET_MENU_URL + "/fa", {
+          cache: "no-store",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }),
+      errorElement: <div>Error</div>,
+      // children: [
+      //   {
+      //     path: "shows/:showId",
+      //     Component: ()=><div>show</div>,
+      //     loader: ({ request, params }) =>
+      //       fetch(`/api/show/${params.id}.json`, {
+      //         signal: request.signal,
+      //       }),
+      //   },
+      // ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
